70149aae416bddd9ca181f363e1ac36d29974069
Maxim Stovba <maxim.stovba@gmail.com>
2022-08-24 08:35:44 +0300

    forecast changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,18 +6,22 @@ const { API_ACCESS_KEY, BASE_URL } = process.env
 const forecast = (latitude, longitude, callback) => {
   const url = `${BASE_URL}/current?access_key=${API_ACCESS_KEY}&query=${latitude},${longitude}&units=m`
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
       callback(
         undefined,
-        response.body.current.weather_descriptions[0] +
+        body.current.weather_descriptions[0] +
           '. It is currently ' +
-          response.body.current.temperature +
-          ' degress out.'
+          body.current.temperature +
+          ' degress out. It feels like ' +
+          body.current.feelslike +
+          ' degress out. The humidity is ' +
+          body.current.humidity +
+          '%.'
       )
     }
   })
